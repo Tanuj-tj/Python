5720eeba6e1d2ed7c5b73b7f48fc88dd693687e8
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2021-03-20 16:19:38 +0530

    Create Merge_Sort.py

---
+++ Merge_Sort.py
@@ -0,0 +1,79 @@
+def merge_sort(list):
+    """
+    Sort a list in ascending order
+    Return a new sorted list
+
+    Divide: Find the midpoint of the list and divide into sublist
+    Conqure: Recursively sort the sublists created in previous step
+    Combine: Merge the sorted sublist created in previous step
+
+    Complexity : O(NlogN)
+    """
+    # Base Condition
+    if len(list) <= 1:
+        return list
+
+    left_half, right_half = split(list)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
+
+    return merge(left,right)
+
+def split(list):
+    """
+    Divide the unsorted list at midpoint into sublists
+    :return: two sublists - left and right
+    Complexity : O(logN)
+    """
+    mid = len(list)//2
+    left = list[:mid]
+    right= list[mid:]
+
+    return left,right
+
+def merge(left,right):
+    """
+    Merges two list (arrays),sorting them in the process
+    :return: A new Merged List
+    Complexity : O(N)
+    """
+    l = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            l.append(left[i])
+            i += 1
+        else:
+            l.append(right[j])
+            j += 1
+
+    while i < len(left):
+        l.append(left[i])
+        i += 1
+    while j < len(right):
+        l.append(right[j])
+        j += 1
+    return l
+
+def verify_sorted(list):
+    n = len(list)
+
+    if n==0 or n==1:
+        return True
+
+    return list[0] < list[1] and verify_sorted(list[1:])
+
+                           # [10, 12, 32, 45, 55, 61, 76, 90]
+                           # [32, 45, 55, 61, 76, 90]
+                           # [55, 61, 76, 90]
+                           # [76, 90]
+                           # []
+
+alist = [45,76,12,32,55,61,90,10]
+l = merge_sort(alist)
+print(alist)
+print(verify_sorted(alist))
+print(l)
+print(verify_sorted(l))
